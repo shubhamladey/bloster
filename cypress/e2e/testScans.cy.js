65c6c8c61d9fdd81b2f958d30f4011daba4e63c7
shubhamladey <shubhamladey7@gmail.com>
2023-11-02 11:20:43 +0530

    added scenarios and func

--- cypress/e2e/testScans.cy.js
+++ cypress/e2e/testScans.cy.js
@@ -3,15 +3,28 @@ const loginPage = require('../support/PageObjects/LoginPage.js')
 const menuHeader = require('../support/PageObjects/MenuHeader.js')
 
 
-describe('template spec', () => {
+describe('Validate New Scan Functionality', () => {
 
   beforeEach(()=>{
     cy.visit(Cypress.env('baseUrl'))
     cy.get(menuHeader.loginButton)
+    loginPage.loginIntoApplication()
 
   })
 
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('Validate scan output using single valid url', () => {
   })
+  it('Validate scan output using  multiple url', () => {
+  })
+  it('Verify scan button is disable when user enter more than limit (i.e 10+ Urls)', () => {
+  })
+  it('Verify scan output is available for all the 10 valid urls entered', () => {
+  })
+  it('Verify scan output for invalid url-without www and .com', () => {
+  })
+  it('Verify scan output for invalid url-without www and .com- invalid urlname containing www and .com', () => {
+  })
+  it('Verify scan output contains the IP address for valid source Url', () => {
+  })
+
 })
--- cypress/support/PageObjects/LoginPage.js
+++ cypress/support/PageObjects/LoginPage.js
@@ -1,5 +1,16 @@
 class LoginPage{
 
+    usernameField = 'input[type="email"]'
+    passwordField ='input[type="password"]'
+    continueButton = 'button:contains("Continue")'
+    loginButton = 'button[type="submit"]'
+
+    loginIntoApplication(){
+        cy.get(this.usernameField).type(Cypress.env('username'))
+        cy.get(this.continueButton).click()
+        cy.get(this.passwordField).type(Cypress.env('password'))
+        cy.get(this.loginButton).click()
+    }
 
 }
 module.exports = new LoginPage
